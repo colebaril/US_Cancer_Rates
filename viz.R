701cb40952604524f4bdc7b72eec3b627cca5a5e
Cole Baril <cole@colebaril.ca>
2023-04-09 21:22:55 -0500

    Add files via upload

---
+++ viz.R
@@ -0,0 +1,31 @@
+library(tidyverse)
+library(here)
+library(janitor)
+library(usmap)
+library(plotly)
+
+df <- read_csv("Raw/data-table.csv") %>% 
+  clean_names() %>% 
+  select(-"url") %>% 
+  drop_na()
+
+label_NA <- c('NA' = '')
+
+NA_labeller <- function(variable,value){
+  return(label_NA[value])
+}
+
+
+
+plot_usmap(data = na.omit(df), values = "rate", color = "black") +
+  scale_fill_viridis_c("Deaths per \n100,000 Pop",
+                       direction = -1) +
+  facet_wrap(~year) +
+  theme(legend.position = c(.8, 0.05),
+        legend.title = element_text("Deaths per \n 100,000 Pop"),
+        legend.key.size = unit(0.9, "cm")) +
+  labs(title = "Number of Cancer Deaths per 100,000 people in the United States",
+       subtitle = "For years for which data is available from the CDC",
+       caption = "Viz: Cole Baril - colebaril.ca | Data: CDC | Software: R")
+
+
