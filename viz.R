d78868ca372fa069ce09bdb0b073a2bcdc44cec6
Cole Baril <cole@colebaril.ca>
2023-04-10 14:05:19 -0500

    Update viz.R
    Add change over time

--- viz.R
+++ viz.R
@@ -34,4 +34,27 @@ plot_usmap(data = na.omit(df), values = "rate", color = "black") +
        subtitle = "For years for which data is available from the CDC",
        caption = "Viz: Cole Baril - colebaril.ca | Data: CDC | Software: R")
 
+# CHANGE OVER TIME ----
+
+df %>% 
+  select(-deaths) %>% 
+  pivot_wider(names_from = year, values_from = rate) %>% 
+  rename("y2020" = "2020",
+         "y2005" = "2005") %>% 
+  mutate(pct_change = (y2020/y2005)*100-100) %>% 
+  select(state, pct_change) %>% 
+  arrange(pct_change) %>% 
+  gt() %>% 
+  tab_header(title = "Percent Change in Cancer Mortality",
+             subtitle = "2005-2020, per 100,000 Pop") %>% 
+  cols_label(
+    state = "State",
+    pct_change = "Percent Change") %>% 
+  cols_align(align = "center",
+             columns = everything()) %>% 
+  fmt_number(columns = pct_change,
+             rows = everything(),
+             decimals = 2) %>% 
+  tab_footnote("Created by Cole Baril - colebaril.ca | Data: CDC | Software: R")
+
 
